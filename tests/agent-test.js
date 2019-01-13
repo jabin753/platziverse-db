ffabbbf6c788398c3a8e37882ce567283bb03d61
jabin753 <jabin753@gmail.com>
2019-01-13 00:33:29 -0600

    Added Agent#createOrUpdate

--- tests/agent-test.js
+++ tests/agent-test.js
@@ -10,8 +10,12 @@ let config = {
   logging: function () {}
 }
 let db = null
-let single = Object.assign({},agentFixtures.single)
+let single = Object.assign({}, agentFixtures.single)
 let id = 1
+let uuid = 'yyy-yyy-yyy'
+let uuidArgs = {
+  where: { uuid }
+}
 let AgentStub = null
 let MetricStub = { belongsTo: sinon.spy() }
 let sandbox = sinon.createSandbox()
@@ -23,9 +27,16 @@ test.beforeEach(async () => {
     hasMany: sandbox.spy()
   }
 
-    //MODEL findById Stub
-    AgentStub.findById = sandbox.stub()
-    AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+  // Model findById Stub
+  AgentStub.findById = sandbox.stub()
+  AgentStub.findById.withArgs(id).returns(Promise.resolve(agentFixtures.byId(id)))
+  // Model findOne Stub
+  AgentStub.findOne = sandbox.stub()
+  AgentStub.findOne.withArgs(uuidArgs).returns(Promise.resolve(agentFixtures.byUuid(uuid)))
+  // Model update Stub
+  AgentStub.update = sandbox.stub()
+  AgentStub.update.withArgs(single, uuidArgs).returns(Promise.resolve(single))
+
   const setupDatabase = proxyquire('../', {
     './models/agent': () => AgentStub,
     './models/metric': () => MetricStub
@@ -49,6 +60,13 @@ test.serial('Agent#findById', async t => {
   t.true(AgentStub.findById.calledWith(id), 'Should be called with that specific id')
 
   t.deepEqual(agent, agentFixtures.byId(id), 'Should be the same')
+})
+
+test.serial('Agent#createOrUpdate - exists', async t => {
+  let agent = await db.Agent.createOrUpdate(single)
+
+  t.true(AgentStub.findOne.called, 'Should be called on model')
+  t.deepEqual(agent, single, 'agent should be the same')
 })
 test.afterEach(t => {
   sandbox && sandbox.restore()
